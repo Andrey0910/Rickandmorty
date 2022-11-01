bc65f79fb28b0bcf5afe7aa481a69f169246951f
Andrey Petrov <petrovaa71@mail.ru>
2022-11-01 23:18:34 +0300

    добавил baseAdpter

--- app/src/main/java/com/example/rickandmorty/ui/recycleview/core/BaseAdapter.kt
+++ app/src/main/java/com/example/rickandmorty/ui/recycleview/core/BaseAdapter.kt
@@ -1,6 +1,7 @@
 package com.example.rickandmorty.ui.recycleview.core
 
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
 import androidx.viewbinding.ViewBinding
@@ -14,10 +15,34 @@ class BaseAdapter(
         parent: ViewGroup,
         viewType: Int
     ): BaseViewHolder<ViewBinding, Item> {
-        TODO("Not yet implemented")
+        val inflater = LayoutInflater.from(parent.context)
+        @Suppress("UNCHECKED_CAST")
+        return fingerprints.find { it.getLoyoutId() == viewType }
+            ?.getViewHolder(inflater, parent)
+            ?.let { it as BaseViewHolder<ViewBinding, Item> }
+            ?: throw IllegalArgumentException("View type not found: $viewType")
     }
 
     override fun onBindViewHolder(holder: BaseViewHolder<ViewBinding, Item>, position: Int) {
-        TODO("Not yet implemented")
+        holder.onBind(currentList[position])
+    }
+
+    override fun onBindViewHolder(
+        holder: BaseViewHolder<ViewBinding, Item>,
+        position: Int,
+        payloads: MutableList<Any>
+    ) {
+        if (payloads.isEmpty()) {
+            super.onBindViewHolder(holder, position, payloads)
+        } else {
+            holder.onBind(currentList[position], payloads)
+        }
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        val item = currentList[position]
+        return fingerprints.find { it.isRelativeItem(item) }
+            ?.getLoyoutId()
+            ?: throw IllegalArgumentException("View type not found: $item")
     }
 }
