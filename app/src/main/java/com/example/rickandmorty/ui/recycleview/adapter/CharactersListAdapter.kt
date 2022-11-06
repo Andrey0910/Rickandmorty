3ee51ab5e76e8c349c461cec28e67847be227a39
Andrey Petrov <petrovaa71@mail.ru>
2022-11-06 16:50:38 +0300

    доработол CharactersListAdapter

--- app/src/main/java/com/example/rickandmorty/ui/recycleview/adapter/CharactersListAdapter.kt
+++ app/src/main/java/com/example/rickandmorty/ui/recycleview/adapter/CharactersListAdapter.kt
@@ -13,8 +13,8 @@ import com.example.rickandmorty.ui.recycleview.core.BaseViewHolder
 import com.example.rickandmorty.ui.recycleview.core.Item
 import com.example.rickandmorty.ui.recycleview.model.CharactersListDataModel
 
-class CharactersListAdapter (private val onItemClick: (CharactersListDataModel) -> Unit) :
-    BaseItem <ItemCharactersListBinding, CharactersListDataModel> {
+class CharactersListAdapter(private val onItemClick: (CharactersListDataModel) -> Unit) :
+    BaseItem<ItemCharactersListBinding, CharactersListDataModel> {
 
     override fun isRelativeItem(item: Item) = item is CharactersListDataModel
 
@@ -28,17 +28,38 @@ class CharactersListAdapter (private val onItemClick: (CharactersListDataModel) 
         return CharactersListViewHolder(binding, onItemClick)
     }
 
-    override fun getDiffUtil(): DiffUtil.ItemCallback<CharactersListDataModel> {
-        TODO("Not yet implemented")
+    override fun getDiffUtil() = diffUtil
+
+    private val diffUtil = object : DiffUtil.ItemCallback<CharactersListDataModel>() {
+        override fun areItemsTheSame(
+            oldItem: CharactersListDataModel,
+            newItem: CharactersListDataModel
+        ) = oldItem.id == newItem.id
+
+        override fun areContentsTheSame(
+            oldItem: CharactersListDataModel,
+            newItem: CharactersListDataModel
+        ) = oldItem.id == newItem.id
+
+        override fun getChangePayload(
+            oldItem: CharactersListDataModel,
+            newItem: CharactersListDataModel
+        ): Any? {
+            when {
+                oldItem.favorite != newItem.favorite -> return newItem.favorite
+            }
+
+            return super.getChangePayload(oldItem, newItem)
+        }
     }
 
     class CharactersListViewHolder(
         binding: ItemCharactersListBinding,
         val onItemClick: (CharactersListDataModel) -> Unit
-    ) : BaseViewHolder<ItemCharactersListBinding, CharactersListDataModel>(binding){
+    ) : BaseViewHolder<ItemCharactersListBinding, CharactersListDataModel>(binding) {
 
         init {
-            binding.root.setOnClickListener{
+            binding.root.setOnClickListener {
                 if (bindingAdapterPosition == RecyclerView.NO_POSITION) return@setOnClickListener
                 onItemClick(item)
             }
@@ -47,19 +68,35 @@ class CharactersListAdapter (private val onItemClick: (CharactersListDataModel) 
         override fun onBind(item: CharactersListDataModel) {
             super.onBind(item)
 
-            with(binding){
+            with(binding) {
+
                 titleLogo.text = item.name
                 speciesText.text = item.species
                 statusText.text = item.status
+
                 Glide.with(imageLogo.context)
                     .load(Uri.parse(item.image))
                     .into(imageLogo)
+
+                if (item.favorite) {
+                    iconFavorite.setImageResource(R.drawable.ic_favorite)
+                } else {
+                    iconFavorite.setImageResource(R.drawable.ic_favorite_black)
+                }
             }
         }
 
         override fun onBind(item: CharactersListDataModel, payloads: List<Any>) {
             super.onBind(item, payloads)
 
+            with(binding) {
+
+                if (item.favorite) {
+                    iconFavorite.setImageResource(R.drawable.ic_favorite)
+                } else {
+                    iconFavorite.setImageResource(R.drawable.ic_favorite_black)
+                }
+            }
         }
     }
 
