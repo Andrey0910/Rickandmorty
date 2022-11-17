8a7ec38e7f8667fc26f5aa3b61244cf71b54cf32
Andrey Petrov <petrovaa71@mail.ru>
2022-11-17 18:02:29 +0300

    доработал snack bar

--- app/src/main/java/com/example/rickandmorty/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/rickandmorty/ui/activity/MainActivity.kt
@@ -2,13 +2,22 @@ package com.example.rickandmorty.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.view.View
+import android.widget.FrameLayout
+import android.widget.TextView
 import androidx.activity.viewModels
+import androidx.core.content.ContextCompat
+import androidx.core.view.marginLeft
+import androidx.core.view.marginRight
 import androidx.lifecycle.lifecycleScope
+import com.example.rickandmorty.Application
 import com.example.rickandmorty.R
 import com.example.rickandmorty.databinding.ActivityMainBinding
 import com.example.rickandmorty.ui.model.app_view_model.NavigationViewModel
 import com.example.rickandmorty.utils.NetworkConnectionManager
 import com.example.rickandmorty.utils.cicerone.Screens.main
+import com.example.rickandmorty.utils.common.extensions.showCustomSnackBar
 import com.github.terrakok.cicerone.Command
 import com.github.terrakok.cicerone.Navigator
 import com.github.terrakok.cicerone.NavigatorHolder
@@ -17,7 +26,6 @@ import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.collectLatest
-import timber.log.Timber
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -54,20 +62,20 @@ class MainActivity : AppCompatActivity() {
         navigationViewModel.onForwardCommandClick(main())
     }
 
-    fun checkInternet(){
+    fun checkInternet() {
 
-        lifecycleScope.launchWhenCreated{
+        lifecycleScope.launchWhenCreated {
             networkConnectionManager.isNetworkConnectedFlow
-                .collectLatest{
-                    if (!it){
+                .collectLatest {
+                    if (!it) {
                         delay(1000)
-                        Snackbar.make(
-                            binding.clRoot,
-                            R.string.network_error_text,
-                            Snackbar.LENGTH_LONG
+
+                        val snackbar = binding.root.showCustomSnackBar(
+                            getString(R.string.network_error_text),
+                            R.drawable.ic_not_internet,
+                            R.drawable.background_snack_bar_gradient
                         )
-                            .setBackgroundTint(R.drawable.background_snack_bar_gradient)
-                            .show()
+                        snackbar.show()
                     }
                 }
         }
--- app/src/main/java/com/example/rickandmorty/utils/common/extensions/View.kt
+++ app/src/main/java/com/example/rickandmorty/utils/common/extensions/View.kt
@@ -1,6 +1,14 @@
 package com.example.rickandmorty.utils.common.extensions
 
+import android.content.Context
+import android.graphics.Color
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import com.example.rickandmorty.databinding.ItemSnackBarBinding
+import com.google.android.material.snackbar.BaseTransientBottomBar
+import com.google.android.material.snackbar.Snackbar
 import dev.chrisbanes.insetter.applyInsetter
 
 fun View.insetterTop() {
@@ -20,3 +28,22 @@ fun View.insetterRecyclerBottom() {
         }
     }
 }
+
+fun View.showCustomSnackBar(text: String, imjRes: Int, res: Int, duration: Int = 3000): Snackbar {
+    val snackBar = Snackbar.make(this, "", duration)
+    val nullParent: ViewGroup? = null
+    val layoutInflater = LayoutInflater.from(context)
+    val binding: ItemSnackBarBinding = ItemSnackBarBinding.inflate(layoutInflater, nullParent, false)
+    binding.tvNotInternet.text = text
+    binding.imNotInternet.setImageDrawable(context.returnDrawable(imjRes))
+    binding.clNotInternet.background = context.returnDrawable(res)
+    snackBar.view.setBackgroundColor(Color.TRANSPARENT)
+    val snackBarLayout = snackBar.view as Snackbar.SnackbarLayout
+    snackBarLayout.setPadding(0, 0, 0, 0)
+    snackBarLayout.addView(binding.root, 0)
+    snackBar.animationMode = BaseTransientBottomBar.ANIMATION_MODE_SLIDE
+    snackBar.show()
+    return snackBar
+}
+
+fun Context.returnDrawable(id: Int) = ContextCompat.getDrawable(this, id)
